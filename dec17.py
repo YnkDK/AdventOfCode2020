bd3be4cb04c5869f7e2d029879f21d91e2258abb
Martin Storgaard Dieu <martin@storgaarddieu.com>
2020-12-17 20:20:38 +0100

    Solution to seventeenth puzzle

--- dec17.py
+++ dec17.py
@@ -12,17 +12,15 @@ class ConwayCubes:
                     origin[0] = x
                     origin[1] = y
                     self._state.add(tuple(origin))
+        self._all_deltas = list(itertools.product([-1, 0, 1], repeat=dimensions))
+        self._all_deltas.remove((0,) * dimensions)
 
     def run_cycle(self):
         new_state = set()
         candidates = self._get_candidates()
         for candidate in candidates:
-            if self.is_active(candidate):
-                should_add_candidate = self._process_active_cube(candidate)
-            else:
-                should_add_candidate = self._process_inactive_cube(candidate)
-
-            if should_add_candidate:
+            neighbour_active_count = self._minimum_sum_or_four_active_neighbours_count(candidate)
+            if neighbour_active_count == 3 or neighbour_active_count + self.is_active(candidate) == 3:
                 new_state.add(candidate)
         self._state = new_state
 
@@ -37,32 +35,20 @@ class ConwayCubes:
         candidates = set()
         for cube in self._state:
             candidates.add(cube)
-            candidates = candidates.union(set(self._neighbour_generator(cube)))
+            for neighbour in self._neighbour_generator(cube):
+                candidates.add(neighbour)
         return candidates
 
-    def _process_active_cube(self, cube):
+    def _minimum_sum_or_four_active_neighbours_count(self, cube):
         neighbour_active_count = 0
         for neighbour in self._neighbour_generator(cube):
             neighbour_active_count += self.is_active(neighbour)
-            if neighbour_active_count > 3:
-                return False
-        return neighbour_active_count in (2, 3)
+            if neighbour_active_count == 4:
+                return neighbour_active_count
+        return neighbour_active_count
 
-    def _process_inactive_cube(self, cube):
-        neighbour_active_count = 0
-        for neighbour in self._neighbour_generator(cube):
-            neighbour_active_count += self.is_active(neighbour)
-            if neighbour_active_count > 3:
-                return False
-        return neighbour_active_count == 3
-
-    @staticmethod
-    def _neighbour_generator(cube):
-        dimensions = len(cube)
-        all_deltas = list(itertools.product([-1, 0, 1], repeat=dimensions))
-        all_deltas.remove((0,) * dimensions)
-
-        for deltas in all_deltas:
+    def _neighbour_generator(self, cube):
+        for deltas in self._all_deltas:
             neighbour = []
             for current, delta in zip(cube, deltas):
                 neighbour.append(current + delta)
