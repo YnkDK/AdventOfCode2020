b0e4267aad6dbaa40d4d0a2ec4bafb22834d9c03
Martin Storgaard Dieu <martin@storgaarddieu.com>
2020-12-22 08:40:43 +0100

    Solution to eighteenth puzzle

--- dec18.py
+++ dec18.py
@@ -1,6 +1,8 @@
 import collections
 import operator
 
+Operator = collections.namedtuple('Operator', ('operator', 'precedence_level'))
+
 
 class ChildMath:
     _OPERATOR = {
@@ -11,51 +13,105 @@ class ChildMath:
     _RIGHT_PARENTHESIS = ')'
 
     def __init__(self):
-        self._output = collections.deque()
-        self._operators = collections.deque()
+        self._output = collections.deque()  # type: collections.deque[int]
+        self._operators = collections.deque()  # type: collections.deque[Operator]
+        self._precedence_levels = {
+            '*': 1,
+            '+': 1,
+            '(': 0
+        }
+        self._left_parenthesis_operator = Operator(
+            ChildMath._LEFT_PARENTHESIS,
+            self._precedence_levels[ChildMath._LEFT_PARENTHESIS]
+        )
 
     def solve(self, expression: str):
+        """Simple implementation of the Shunting-yard algorithm with the rules given by Advent of Code.
+
+        The state is clean after each execution, so this method can be called multiple times.
+
+        See `Wikipedia: Shunting-yard algorithm <https://en.wikipedia.org/wiki/Shunting-yard_algorithm>`_
+
+        :param expression: The expression to be evaluated.
+        :return: The result after evaluation.
+        """
         for token in expression:
             if token == " ":
                 continue
+
             try:
+                # if the token is a number, then:
+                #     push it to the output queue.
                 self._output.append(int(token))
             except ValueError:
                 self._handle_non_int(token)
 
+        # while there are still operator tokens on the stack:
         while len(self._operators) > 0:
+            # pop the operator from the operator stack onto the output queue.
             self._execute()
 
         assert len(self._output) == 1, 'Mismatch between operators and outputs'
         return self._output.pop()
+
+    def switch_to_advanced_math(self):
+        """Sets the addition (+) operator to have higher precedence level than multiplication (*) operator.
+        """
+        self._precedence_levels = {
+            '*': 2,
+            '+': 1,
+            '(': 0
+        }
+        self._left_parenthesis_operator = Operator(
+            ChildMath._LEFT_PARENTHESIS,
+            self._precedence_levels[ChildMath._LEFT_PARENTHESIS]
+        )
 
     def _handle_non_int(self, token: str):
         try:
             op = ChildMath._OPERATOR[token]
         except KeyError:
             if token == ChildMath._RIGHT_PARENTHESIS:
-                while len(self._operators) > 0 and self._operators[-1] != ChildMath._LEFT_PARENTHESIS:
+                # while the operator at the top of the operator stack is not a left parenthesis:
+                while self._operators[-1].operator != ChildMath._LEFT_PARENTHESIS:
+                    # pop the operator from the operator stack onto the output queue.
                     self._execute()
-                assert self._operators[-1] == ChildMath._LEFT_PARENTHESIS
-                self._operators.pop()
+                # pop the operator from the operator stack and discard it
+                left_parenthesis = self._operators.pop()
+                assert left_parenthesis.operator == ChildMath._LEFT_PARENTHESIS
             else:
                 assert token == ChildMath._LEFT_PARENTHESIS, f'Expected (, but got "{token}"'
-                self._operators.append(token)
+                # else if the token is a left parenthesis (i.e. "("), then:
+                #     push it onto the operator stack.
+                self._operators.append(self._left_parenthesis_operator)
         else:
-            while len(self._operators) > 0 and self._operators[-1] != ChildMath._LEFT_PARENTHESIS:
+            op = Operator(op, self._precedence_levels[token])
+            # while ((there is an operator at the top of the operator stack)
+            #       and ((the operator at the top of the operator stack has greater precedence)
+            #           or (the operator at the top of the operator stack has equal precedence))
+            #       and (the operator at the top of the operator stack is not a left parenthesis)):
+            while len(self._operators) > 0 \
+                    and self._operators[-1].precedence_level <= op.precedence_level \
+                    and self._operators[-1].operator != ChildMath._LEFT_PARENTHESIS:
+                # pop operators from the operator stack onto the output queue.
                 self._execute()
+            # push it onto the operator stack.
             self._operators.append(op)
 
     def _execute(self):
         op = self._operators.pop()
         b = self._output.pop()
         a = self._output.pop()
-        res = op(a, b)
+        res = op.operator(a, b)
         self._output.append(res)
 
 
 def solve(expressions: list):
     child_math = ChildMath()
+    total = sum(map(child_math.solve, expressions))
+    yield total
+
+    child_math.switch_to_advanced_math()
     total = sum(map(child_math.solve, expressions))
     yield total
 
@@ -65,3 +121,4 @@ if __name__ == '__main__':
         dec18 = f.read().split('\n')
     solutions = solve(dec18)
     print('Part 1:', next(solutions))
+    print('Part 2:', next(solutions))
--- dec18_tests.py
+++ dec18_tests.py
@@ -4,6 +4,14 @@ import unittest
 
 from dec18 import ChildMath
 
+EXAMPLES = (
+    '1 + (2 * 3) + (4 * (5 + 6))',
+    '2 * 3 + (4 * 5)',
+    '5 + (8 * 3 + 9 + 3 * 4 * 3)',
+    '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))',
+    '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'
+)
+
 
 class December18Test(unittest.TestCase):
     def test_no_parenthesis(self):
@@ -14,8 +22,28 @@ class December18Test(unittest.TestCase):
         self.assertEqual(71, solution)
 
     def test_parenthesis(self):
+        expected = (51, 26, 437, 12240, 13632)
         cm = ChildMath()
 
-        solution = cm.solve('1 + (2 * 3) + (4 * (5 + 6))')
+        for expression, expected_result in zip(EXAMPLES, expected):
+            with self.subTest():
+                solution = cm.solve(expression)
+                self.assertEqual(expected_result, solution)
 
-        self.assertEqual(51, solution)
+    def test_advanced_no_parenthesis(self):
+        ch = ChildMath()
+        ch.switch_to_advanced_math()
+
+        solution = ch.solve('1 + 2 * 3 + 4 * 5 + 6')
+
+        self.assertEqual(231, solution)
+
+    def test_advanced(self):
+        expected = (51, 46, 1445, 669060, 23340)
+        cm = ChildMath()
+        cm.switch_to_advanced_math()
+
+        for expression, expected_result in zip(EXAMPLES, expected):
+            with self.subTest():
+                result = cm.solve(expression)
+                self.assertEqual(expected_result, result)
